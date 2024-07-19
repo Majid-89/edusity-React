2b645f1ef860def7c73880a1e53dad0568b778e3
Abdul-majid <noble.majid1@gmail.com>
2024-07-19 10:42:50 +0500

    All files uploaded

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../Navbar/Navbar.css"
+import Logo from "../../assets/logo.png"
+
+const Navbar = () => {
+    return (
+        <nav>
+            <img src={Logo} alt="" />
+        </nav>
+    )
+}
+
+export default Navbar

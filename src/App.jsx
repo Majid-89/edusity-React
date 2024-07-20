f894e3c5124af126d1cb4e8da92f0b9e0375d5d0
Abdul-majid <noble.majid1@gmail.com>
2024-07-20 10:17:46 +0500

    Programs section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
+import Program from './Components/Programs/Program'
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Program />
+      </div>
+
     </>
   )
 }

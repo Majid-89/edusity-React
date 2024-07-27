54e9de55e6256bdb5fd0423f23f681f69845ada7
Abdul-majid <noble.majid1@gmail.com>
2024-07-27 19:52:39 +0500

    Full website completed

--- src/Components/VideoPlayer/VideoPlayer.jsx
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Video from "../../assets/Collage-video.mp4"
 import "./VideoPlayer.css"
 
 const VideoPlayer = ({ setPlayicon, playicon }) => {
+
+    const player = useRef(null)
+
+    const closePlayer = (e) => {
+        console.log(e.target)
+        if (e.target === player.current) {
+            setPlayicon(false)
+        }
+    }
+
     return (
-        <div className={`video-player ${playicon ? ' ' : "hide"}`} onClick={() => setPlayicon(false)}>
+        <div className={`video-player ${playicon ? ' ' : "hide"}`} ref={player} onClick={closePlayer}>
             <video src={Video} autoPlay muted controls></video>
         </div>
     )
